14992185ebf2f60997b538e99d4bbdcc84860505
amit <amitchakra3@gmail.com>
2019-04-29 00:53:39 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+const port = 3000;
+hbs.registerPartials(__dirname+ '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname+ '/public'));
+app.use((req, res, next) => {
+next();
+});
+
+hbs.registerHelper('getCurrentyear', () =>{
+return  new Date().getFullYear();
+});
+hbs.registerHelper('screamit', (text) =>{
+    return  text.toUpperCase();
+    });
+app.get('/', (req, res) => {
+    // res.send('<h1>hello world</h1>');
+    res.render('home.hbs', {
+        pagetitle: 'home page',
+        pagetext: 'welcome to home page',
+        
+    });
+});
+app.get('/about',(req, res) =>{
+    res.render('about.hbs', {
+        pagetitle: 'about us apge',
+       
+    });
+});
+app.get('/bad', (req, res) => {
+res.send({
+ errorMessage: 'unable to handle request'
+})
+});
+
+app.listen(port); 
